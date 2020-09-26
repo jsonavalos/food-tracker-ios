b2882d60fc34e6ede2133177e02702b247153c3f
jsonavalos <jsonavalos>
2020-09-26 12:15:12 -0700

    Genesis

---
+++ Food Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Tracker
+//
+//  Created by Jason Avalos on 9/25/20.
+//  Copyright © 2020 Jason Avalos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
